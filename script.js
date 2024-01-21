1934d2d1aa5a4d42872b51d008a08617bcff2d77
AAliasghar <a.asgharinia@yahoo.com>
2024-01-21 23:40:03 +0100

    Js Deposit/Witdraw

--- script.js
+++ script.js
@@ -1,1 +1,97 @@
-"use strict";
+'use strict';
+
+// DATA
+const account1 = {
+  owner: 'Ali Asgharinia',
+  movements: [1200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Nazi Pergi',
+  movements: [7000, 340, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Sam Rio',
+  movements: [2000, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Anna Demis',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+// --- function to display Movements Deposit/Witdraw
+
+const displayMovements = function (movements) {
+  // Empty existing movements
+  containerMovements.innerHTML = '';
+
+  // Loop through array of Data received and Display the content in movements class in HTML
+  movements.forEach(function (mov, i) {
+    // -Setting type of movement
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+     <div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+          ${i + 1} ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}€</div>
+      </div>
+      `;
+    // Inserting in html class after element tag start so any child element will display after previous one therefore latest stay all top
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+// ---Create userNames-----------
+const createUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    // looping throught accounts and each account userName is...
+    account.userName = account.owner // adding new property useName to object of each account element
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
